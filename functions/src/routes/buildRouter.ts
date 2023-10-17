9f80b28655a7fc12656f2b0c5927548b986288c6
Mitch Cuckovich <cuckovich94@gmail.com>
2023-10-17 00:53:43 -0400

    edit builds

--- functions/src/routes/buildRouter.ts
+++ functions/src/routes/buildRouter.ts
@@ -25,8 +25,29 @@ buildRouter.post("/builds", async (req, res) => {
   try {
     const client = await getClient();
     const newBuild: Build = req.body;
-    client.db().collection<Build>("builds").insertOne(newBuild);
-    res.status(201).json(newBuild);
+    const { title, kitColor, images } = newBuild;
+    console.log(newBuild);
+    const existingBuild = await client
+      .db()
+      .collection<Build>("builds")
+      .findOne({ title, kitColor });
+
+    if (existingBuild) {
+      // If a document with the same title and kit color exists, update the images property
+      existingBuild.images = [...existingBuild.images, ...images];
+      await client
+        .db()
+        .collection<Build>("builds")
+        .updateOne(
+          { title, kitColor },
+          { $set: { images: existingBuild.images } }
+        );
+      res.status(201).json(existingBuild);
+    } else {
+      // If no document exists with the same title and kit color, create a new document
+      await client.db().collection<Build>("builds").insertOne(newBuild);
+      res.status(201).json(newBuild);
+    }
   } catch (err) {
     errorResponse(err, res);
   }
@@ -44,6 +65,24 @@ buildRouter.put("/builds/:id", async (req, res) => {
       .replaceOne({ _id }, build);
     if (result.modifiedCount) {
       res.status(200).json(build);
+    } else {
+      res.status(404).json({ message: `Build with id ${_id} not found` });
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+buildRouter.delete("/builds/:id", async (req, res) => {
+  try {
+    const _id: ObjectId = new ObjectId(req.params.id);
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<Build>("builds")
+      .deleteOne({ _id });
+    if (result.deletedCount) {
+      res.sendStatus(204);
     } else {
       res.status(404).json({ message: `Build with id ${_id} not found` });
     }
